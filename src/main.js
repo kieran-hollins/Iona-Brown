bfdbc5b672a39a27562f322c7a31d871e27dba33
Kieran Hollins <khollins123@gmail.com>
2023-03-06 21:11:09 +0000

    adjusted menu botton

--- src/main.js
+++ src/main.js
@@ -1,22 +1,36 @@
 let burger = document.getElementsByClassName("hamburger")[0];
 let menuBg = document.getElementsByClassName("menu-bg")[0];
+let homeBg = document.getElementById("home-bg");
+let menuToggle = false;
 
 burger.addEventListener("click", openMenu);
 
 function openMenu() {
-    menuBg.style.color = "white";
-    let op = 0.1; // initial opacity
-    menuBg.style.opacity = op;
-    menuBg.style.display = "grid";
+    if (!menuToggle) {
+        menuBg.style.color = "white";
+        let op = 0.1; // initial opacity
+        menuBg.style.opacity = op;
+        menuBg.style.display = "flex";
+        homeBg.style.display = "flex";
 
-    let timer = setInterval(function () {
-        if (op >= 1){
-            clearInterval(timer);
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
-    }, 10);
+        let timer = setInterval(function () {
+            if (op >= 1){
+                clearInterval(timer);
+            }
+            menuBg.style.opacity = op;
+            menuBg.style.filter = 'alpha(opacity=' + op * 100 + ")";
+            op += 0.1;
+        }, 500);
 
+        menuToggle = true;
+    }
+
+    else {
+        let op = 0.1;
+        menuBg.style.opacity = op;
+        menuBg.style.display = "none";
+
+        menuToggle = false;
+    }
     
 }
