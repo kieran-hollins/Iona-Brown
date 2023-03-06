10f858e291172a9a2a2283e4455473ac4a772cf9
Kieran Hollins <khollins123@gmail.com>
2023-03-06 21:19:24 +0000

    button now enables menu with transition

--- src/main.js
+++ src/main.js
@@ -7,10 +7,9 @@ burger.addEventListener("click", openMenu);
 
 function openMenu() {
     if (!menuToggle) {
-        menuBg.style.color = "white";
+
         let op = 0.1; // initial opacity
         menuBg.style.opacity = op;
-        menuBg.style.display = "flex";
         homeBg.style.display = "flex";
 
         let timer = setInterval(function () {
@@ -20,15 +19,23 @@ function openMenu() {
             menuBg.style.opacity = op;
             menuBg.style.filter = 'alpha(opacity=' + op * 100 + ")";
             op += 0.1;
-        }, 500);
+        }, 20);
 
         menuToggle = true;
     }
 
     else {
-        let op = 0.1;
+        let op = 1;
         menuBg.style.opacity = op;
-        menuBg.style.display = "none";
+
+        let timer = setInterval(function () {
+            if (op < 0.1){
+                clearInterval(timer);
+            }
+            menuBg.style.opacity = op;
+            menuBg.style.filter = 'alpha(opacity=' - op * 100 + ")";
+            op -= 0.1;
+        }, 20);
 
         menuToggle = false;
     }
